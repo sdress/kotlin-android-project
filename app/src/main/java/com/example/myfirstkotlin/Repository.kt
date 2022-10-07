4c09e767c42a24825963bcd39bc15a4b43d2fb21
sdress <sarah.dresser1@gmail.com>
2022-10-07 11:44:45 +0200

    handling nullability step

--- app/src/main/java/com/example/myfirstkotlin/Repository.kt
+++ app/src/main/java/com/example/myfirstkotlin/Repository.kt
@@ -1,7 +1,11 @@
 import java.util.ArrayList
 
-class Repository private constructor() {
+object Repository  {
+
     private var users: MutableList<User>? = null
+    fun getUsers(): List<User>? {
+        return users
+    }
 
     // keeping the constructor private to enforce the usage of getInstance
     init {
@@ -14,15 +18,10 @@ class Repository private constructor() {
         users.add(user3)
     }
 
-    fun getUsers(): List<User>? {
-        return users
-    }
-
     val formattedUserNames: List<String>
         get() {
-            val userNames: MutableList<String> = ArrayList(
-                users!!.size
-            )
+            val userNames: MutableList<String> =
+                ArrayList(users!!.size)
             for (user in users) {
                 var name: String
                 name = if (user.getLastName() != null) {
@@ -40,21 +39,18 @@ class Repository private constructor() {
             }
             return userNames
         }
-
-    companion object {
-        private var INSTANCE: Repository? = null
-        val instance: Repository?
-            get() {
-                if (INSTANCE == null) {
-                    synchronized(Repository::class.java) {
-                        if (INSTANCE == null) {
-                            INSTANCE = Repository()
-                        }
-                    }
-                }
-                return INSTANCE
-            }
+    // keeping the constructor private to enforce the usage of getInstance
+    init {
+        val user1 = User("Jane", "")
+        val user2 = User("John", null)
+        val user3 = User("Anne", "Doe")
+        users = ArrayList<Any?>()
+        users.add(user1)
+        users.add(user2)
+        users.add(user3)
     }
 
 
 }
+
+val formattedUserNames = Repository.formattedUserNames
